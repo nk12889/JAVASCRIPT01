3c5976c68642f8731c17dfe7590165207ff129d4
Nitish <nk12889@gmail.com>
2023-07-20 09:01:41 +0530

    my final commit

--- demo.js
+++ demo.js
@@ -1,1 +1,7 @@
-console.log("I love github")
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+// title. = 'blue';
+items.style.backgroundColor = 'green'
+
+var list= document.getElementById('items');
+list.style.fontWeight = 'bold';
