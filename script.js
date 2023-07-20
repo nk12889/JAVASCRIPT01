da092e12b5759578e7bdf7209b105a2c05e2d87f
Nitish <nk12889@gmail.com>
2023-07-20 23:36:21 +0530

    new commit

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     const userDetailsForm = document.getElementById('userDetailsForm');
+  
+//     userDetailsForm.addEventListener('submit', function(event) {
+//       event.preventDefault();
+  
+//       const nameInput = document.getElementById('name').value;
+//       const emailInput = document.getElementById('email').value;
+//       const ageInput = document.getElementById('age').value;
+//   JSON
+//       // Create an object to store the user details
+//       const userDetails = {
+//         name: nameInput,
+//         email: emailInput,
+//         age: ageInput,
+//       };
+  
+//       // Store the user details object in the local storage
+//       localStorage.setItem('userDetails', JSON.stringify(userDetails));
+  
+//       // Clear the form inputs
+//       userDetailsForm.reset();
+  
+//       alert('User details have been successfully stored in the local storage.');
+//     });
+//   });
 document.addEventListener('DOMContentLoaded', function() {
     const userDetailsForm = document.getElementById('userDetailsForm');
   
@@ -15,8 +41,14 @@ document.addEventListener('DOMContentLoaded', function() {
         age: ageInput,
       };
   
-      // Store the user details object in the local storage
-      localStorage.setItem('userDetails', JSON.stringify(userDetails));
+      // Get existing user details from LocalStorage
+      let existingUsers = JSON.parse(localStorage.getItem('userDetails')) || [];
+  
+      // Add the new user details to the existing array
+      existingUsers.push(userDetails);
+  
+      // Store the updated array of user details in the local storage
+      localStorage.setItem('userDetails', JSON.stringify(existingUsers));
   
       // Clear the form inputs
       userDetailsForm.reset();
