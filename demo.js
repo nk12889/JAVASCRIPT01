354e405a9ef4abfe2db8814a9a07cf58bfe90c9d
Nitish <nk12889@gmail.com>
2023-07-20 09:56:41 +0530

    finl ccommit 3

--- demo.js
+++ demo.js
@@ -3,16 +3,27 @@
 // // title. = 'blue';
 // items.style.backgroundColor = 'green'
 
-var list= document.getElementById('items');
-list.style.fontWeight = 'bold';
+// var list= document.getElementById('items');
+// list.style.fontWeight = 'bold';
 // var header = document.querySelector('#main-header');
 // header.style.borderBottom = 'solid 4px #ccc';
 
 // var input= document.getElementsByClassName('list-group');
 // input.appendChild='item 5';
 
-var node= document.getElementById('items')
-document.createElement("li"); 
-const textnode = document.createTextNode("Item 5");
- node.appendChild(textnode);
+// var node= document.getElementById('items')
+// document.createElement("li"); 
+// const textnode = document.createTextNode("Item 5");
+//  node.appendChild(textnode);
  
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items[1]);
+// items[1].style.backgroundColor = "green"
+// items[2].style.display = 'none';
+
+
+
+
+
+var list = document.querySelector('li');
+list.style.backgroundColor="green"
