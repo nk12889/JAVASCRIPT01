57c4b060c5706fbc26425bd5eb94bf150a6d808f
Nitish <nk12889@gmail.com>
2023-07-20 09:35:41 +0530

    new updated code

--- demo.js
+++ demo.js
@@ -1,7 +1,18 @@
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-// title. = 'blue';
-items.style.backgroundColor = 'green'
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// // title. = 'blue';
+// items.style.backgroundColor = 'green'
 
 var list= document.getElementById('items');
 list.style.fontWeight = 'bold';
+// var header = document.querySelector('#main-header');
+// header.style.borderBottom = 'solid 4px #ccc';
+
+// var input= document.getElementsByClassName('list-group');
+// input.appendChild='item 5';
+
+var node= document.getElementById('items')
+document.createElement("li"); 
+const textnode = document.createTextNode("Item 5");
+ node.appendChild(textnode);
+ 
